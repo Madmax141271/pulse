49f55f0fa378e1b1730071be1f1941a431259037
Maks <pan71@tut.by>
2021-05-11 11:30:38 +0300

    10.05.2021

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,25 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        // adaptiveHeight: true,
+        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/right.png"></button>',
-        variableWidth: true,
         responsive: [
             {
                 breakpoint: 992,
                 settings: {
                     dots: true,
-                    arrows: false,
-                    infinite: true,
-                    variableWidth:true,
+                    arrows: false
                 }
-                
-            },
-           
+            }
         ]
-
     });
-
+    
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass
-          ('catalog__content_active').eq($(this).index()).addClass
-          ('catalog__content_active');
-      });
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 
     function toggleSlide(item) {
         $(item).each(function(i) {
@@ -35,11 +27,56 @@ $(document).ready(function(){
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            })
+            });
         });
-    };
+    }
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
-});
 
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+    $('#consultation-form').validate();
+    $('#consultation form').validate({
+        rules: {
+            name: "reqired",
+            phone: "reqired",
+            email: {
+                reqired: true,
+                email: true
+            }
+        }
+    });
+    $('#order form').validate({
+        rules: {
+            name: "reqired",
+            phone: "reqired",
+            email: {
+                reqired: true,
+                email: true
+            }          
+        },
+        messages: {
+            name: "Пожалуйста введите свое имя",
+            phone: "Укажите свой тф",
+            email: {
+              required: "Нам необходим адрес вашей почты",
+              email: "Фомат вашей почты name@domain.com"
+            }
+        }
+    });
+    
+});
